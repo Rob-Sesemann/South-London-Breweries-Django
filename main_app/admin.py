627a3553666cfdab26bf9fdd852421836ae68e8a
sesemann <robsesemann@gmail.com>
2022-09-28 16:38:55 +0100

    delete working made sure the main_app/brewery_ .html folder was written correctly

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Brewery
+from .models import Brewery, Specialbrew
 
 # Register your models here.
 
 admin.site.register(Brewery)
+admin.site.register(Specialbrew)
--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-
+VARIETY = [
+    ('DIPA', 'Double Indian Pale Ale'),
+    ('IPA', 'Indian Pale Ale'),
+    ('PA', 'Pale Ale'),
+    ('L', 'Lager'),
+    ('STO', 'Stout'),
+    ('WHA', 'Wheat Ale'),
+    ('SIP', 'Session Indian Pale Ale'),
+    ('TIP', 'Triple Indian Pale Ale'),
+    ('SOUR', 'Sour ale'),
+]
 class Brewery(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=150)
@@ -13,3 +23,13 @@ class Brewery(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs = {'brewery_id': self.id})
+
+class Specialbrew(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    date = models.DateField()
+    variety = models.CharField(max_length=4, choices=VARIETY, default=VARIETY[0][0][0][0][0][0][0][0][0]) # DIPA IPA PA L STO WHA SIP TIP SOUR
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
